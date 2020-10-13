104402d84257a868fd7c15da3751f3d787d74421
Dan <danya624@gmail.com>
2020-10-13 20:40:50 +0300

    v 3.0

--- src/Boot/Main.java
+++ src/Boot/Main.java
@@ -1,34 +1,17 @@
 package Boot;
+import Items.Text;
 import Parser.Parser;
 import Reader.Reader;
-import Items.*;
-import Test.Test;
-
 import java.io.IOException;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Reader reader = new Reader();
         String sourceText = reader.read("src/text.txt");
 
-
         Parser parser = new Parser();
-        List<Word> words = parser.parseToWords(sourceText);
-
-
-         for (int i=0; i<words.size(); i++){
-            System.out.println(words.get(i).getText());
-        }
-
-         System.out.println(parser.wordsToText(words, sourceText));
-
-         String text = parser.wordsToText(words, sourceText);
-
-         Test test = new Test();
-         System.out.println(test.initiateTest(text, sourceText));
-
-         System.out.println(parser.deleteConsonants(words, 9, sourceText));
-
+        Text text = parser.parseText(sourceText);
+        System.out.println(text);
     }
 }
